9174f8cb92a53b5286a326000277e67cd6d2366a
JackLillie <42320657+JackLillie@users.noreply.github.com>
2022-02-17 14:47:32 +0000

    Add filter sidebar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import Checkout from "./components/Checkout";
 import LoginModal from "./components/login/LoginModal";
 import Header from "./components/header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Blurb from "./components/Blurb";
+import Filters from "./components/Filters";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Blurb
+        title="Shop Plants"
+        body="Revamp your style with the latest designer trends in men’s clothing or achieve a perfectly curated wardrobe thanks to our line-up of timeless pieces. "
+      />
+      <Filters />
       <Routes>
-        <Route index element={<Component />} />
         <Route path="checkout" element={<Checkout />} />
         <Route path="login" element={<LoginModal />} />
       </Routes>
