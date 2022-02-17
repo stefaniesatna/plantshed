8756f3f86a6f8bfd400d792c05b75f825e31467d
Stefanie Satna <heystef@fb.com>
2022-02-17 15:49:48 +0000

    There we go

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import Checkout from "./components/Checkout";
 import LoginModal from "./components/login/LoginModal";
 import Header from "./components/header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
 import LoginWithEmail from "./components/login/LoginWithEmail";
-=======
-import Blurb from "./components/Blurb";
-import Filters from "./components/Filters";
->>>>>>> cde5a8c3b5ed51a296aea8ee933e90a32a169c43
 
 function App() {
   return (
--- src/components/login/LoginModal.js
+++ src/components/login/LoginModal.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Facebook, Google, MailOutline } from "@mui/icons-material";
 import "../../styles/Login.css";
 
@@ -25,30 +24,3 @@ export default function LoginModal({handleSelect}) {
     </div>
   );
 }
-=======
-import LoginButton from "./LoginButton"
-
-export default function LoginModal(){
-
-    const loginWays = [{name: "Facebook", icon: "FacebookIcon"}, {name: "Google", icon: "GoogleIcon"}, {name: "Email", icon: "MailOutlineIcon"}];
-
-    const style = {
-        width: "460px", 
-        backgroundColor: "#ffffff",
-        borderColor: "red",
-        borderStyle: "solid",
-        borderWidth: 1,
-        padding: 38,
-    }
-
-    const loginButtons = loginWays.map(way => <LoginButton way={way.name} icon={way.icon} />)
-
-    return (
-        <div style={style}>
-            <h1>Welcome Back</h1>
-            <h2>Login to continue</h2>
-            {loginButtons}
-        </div>
-    )
-}
->>>>>>> cde5a8c3b5ed51a296aea8ee933e90a32a169c43
