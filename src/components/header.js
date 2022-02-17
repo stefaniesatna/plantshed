942fb8a202d61c900f75fa20ed1e41f970cc22ed
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2022-02-17 14:49:32 +0000

    Working links in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,25 @@
 import Basket from "./basket";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className='header'>
-        <span className='headerLinks'>
-            <p className='headerLink'>Coffee</p>
-            <p className='headerLink'>Plants</p>
-            <p className='headerLink'>About</p>
-        </span>
+    <div className="header">
+      <span className="headerLinks">
+        <p className="headerLink">Coffee</p>
+        <p className="headerLink">Plants</p>
+        <p className="headerLink">About</p>
+      </span>
 
-        <img src='/img/ps-logo.svg' height='40px'/>
+      <img src="/img/ps-logo.svg" height="40px" />
 
-        <span className='headerAccount'>
-            <Basket />
-            <p className='headerLink'>Login</p>
-        </span>
+      <span className="headerAccount">
+        <Link className="headerLink" to="checkout">
+          <Basket />
+        </Link>
+        <Link className="headerLink" to="login">
+          Login
+        </Link>
+      </span>
     </div>
-  )
+  );
 }
