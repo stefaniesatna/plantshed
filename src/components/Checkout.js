e6874f56a9d630ad74da4ce8c63f52d0688a704f
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2022-02-17 12:35:25 +0000

    Make styling for card details more similar to mockup

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,24 +1,40 @@
-import '../styles/Checkout.css';
+import "../styles/Checkout.css";
 
 const Checkout = () => {
+  const visaImage = "https://seeklogo.com/images/V/visa-logo-6F4057663D-seeklogo.com.png";
+
   return (
-    <div style={{ backgroundColor: "pink" }}>
-      <h1>Your Basket</h1>
-      <form>
-        <div>
-          <label>First Name</label>
-          <input type="text" className="firstName" id="firstName" name="firstName" required />
-          <label>Last Name</label>
-          <input type="text"  className="lastName" id="lastName" name="lastName" required />
+    <div className="checkout">
+      <h1>Checkout</h1>
+      <form className="checkout-container">
+        <h3 className="heading-3">Payment Details</h3>
+        <Input label="Cardholder's Name" type="text" name="name" />
+        <Input
+          label="Card Number"
+          type="number"
+          name="card_number"
+          imgSrc={visaImage}
+        />
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+            <Input label="Expiration Date" type="month" name="exp_date" />
+            <Input label="CVV" type="number" name="cvv" />
         </div>
-      <CheckoutButton text="Continue to Shipping"/>
+        <button className="checkout-btn" type="button">
+       {"Continue to Shipping"}
+      </button>      
       </form>
     </div>
   );
 };
 
-const CheckoutButton = (props) => (
-    <button className="checkout-btn" type="button">{props.text}</button>
-)
+const Input = (props) => (
+  <div className="input">
+    <label>{props.label}</label>
+    <div className="input-field">
+      <input type={props.type} name={props.name} />
+      <img src={props.imgSrc} />
+    </div>
+  </div>
+);
 
 export default Checkout;
